32ec8a1f8e408c8cc92888026cc725f1af11193d
Julio Lab <guillermo_julio.betancourt_vera@onera.fr>
2024-08-20 19:07:21 +0200

    comment prints that are not needed anymore

--- src/modules/mc_pos_control/PositionControl/PositionControl.cpp
+++ src/modules/mc_pos_control/PositionControl/PositionControl.cpp
@@ -185,8 +185,8 @@ void PositionControl::_positionControlMR(const float dt)
 	std::cout << std::endl;
 	// std::cout << "_thrust_sp_1: " <<   _thr_sp(0) << "  " <<    _thr_sp(1) << "  " <<   _thr_sp(2) << std::endl;
 	// std::cout << "     _pos_sp: " <<   _pos_sp(0) << "  " <<    _pos_sp(1) << "  " <<   _pos_sp(2) << std::endl;
-	std::cout << "        _pos: " <<      _pos(0) << "  " <<       _pos(1) << "  " <<      _pos(2) << std::endl;
-	std::cout << "       Eular: " <<         roll << "  " << pitch << "  " <<  yaw << std::endl;
+	//std::cout << "        _pos: " <<      _pos(0) << "  " <<       _pos(1) << "  " <<      _pos(2) << std::endl;
+	//std::cout << "       Eular: " <<         roll << "  " << pitch << "  " <<  yaw << std::endl;
 	// // std::cout << "         e_p: " <<       e_p(0) << "  " <<       e_p(1) << "  " <<       e_p(2) << std::endl;
 	// // std::cout << "         r_p: " <<       r_p(0) << "  " <<       r_p(1) << "  " <<       r_p(2) << std::endl;
 
